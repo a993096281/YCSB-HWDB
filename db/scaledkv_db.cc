bac9efef294963bc40e02320c09ae297289dd6aa
a993096281 <993096281@qq.com>
2019-09-09 19:03:12 +0800

    test

--- db/scaledkv_db.cc
+++ db/scaledkv_db.cc
@@ -10,16 +10,6 @@
 using namespace std;
 
 namespace ycsbc {
-    static inline void fillchar8wirhint64(char *key, uint64_t value) {
-        key[0] = ((char)(value >> 56)) & 0xff;
-        key[1] = ((char)(value >> 48)) & 0xff;
-        key[2] = ((char)(value >> 40) )& 0xff;
-        key[3] = ((char)(value >> 32)) & 0xff;
-        key[4] = ((char)(value >> 24)) & 0xff;
-        key[5] = ((char)(value >> 16)) & 0xff;
-        key[6] = ((char)(value >> 8)) & 0xff;
-        key[7] = ((char)value) & 0xff;
-    }
     ScaledKV::ScaledKV(const char *dbfilename, utils::Properties &props) :noResult(0){
     
         //set option
--- test_sh/scaledkv_400M.sh
+++ test_sh/scaledkv_400M.sh
@@ -2,7 +2,7 @@
 
 workload="workloads/400M_load.spec"
 dbpath="/home/lzw/ceshi2"
-moreworkloads="workloads/50M_workloada.spec:workloads/50M_workloadb.spec:workloads/50M_workloadc.spec:workloads/50M_workloadd.spec:workloads/1M_workloade.spec:workloads/50M_workloadef.spec"
+moreworkloads="workloads/50M_workloada.spec:workloads/50M_workloadb.spec:workloads/50M_workloadc.spec:workloads/50M_workloadd.spec:workloads/1M_workloade.spec:workloads/50M_workloadf.spec"
 
 #./ycsbc -db scaledkv -dbpath $dbpath -threads 1 -P $workload -load true -run true -dbstatistics true
 
